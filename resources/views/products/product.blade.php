dfbf50ada059cc20535329e5257babec2c916e87
Keshav acharya <acharyakeshab20@gmail.com>
2022-12-30 16:32:13 +0545

    Adding products details in Products page

---
+++ resources/views/products/product.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.cms');
+<div class="container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quos suscipit nemo aperiam a, eum officia accusantium consequuntur facere nam ipsum corrupti optio nesciunt? Incidunt fugiat tempore distinctio animi aspernatur!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores qui officiis cumque excepturi enim nihil et tempore. Aspernatur aut incidunt ducimus soluta nobis? Velit dolor vel aspernatur minus impedit in.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nobis eaque doloribus odit. Explicabo rem voluptatem, perferendis culpa nihil velit dicta libero nesciunt at reiciendis quod suscipit optio magni nisi?
+</div>
