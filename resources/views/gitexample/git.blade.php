fa4eed473cdf60426c0eeb75b7dfea8f747275ef
Keshav acharya <acharyakeshab20@gmail.com>
2022-12-30 16:49:41 +0545

    making gits brach

---
+++ resources/views/gitexample/git.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+<div class="row">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum officia accusamus blanditiis amet quod obcaecati velit cupiditate molestiae, repudiandae omnis recusandae neque minus incidunt illo eum. Neque explicabo animi quis.
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quia asperiores dolorem inventore eum similique magnam eos, assumenda omnis dolor, aut perspiciatis maxime, quod ad! Odit provident placeat illo ratione.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti earum modi eius ducimus laborum, quaerat aperiam quidem dolor, dolores doloribus est, molestiae ea. Culpa pariatur, quam quas laudantium minus dolore.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur error, dolor libero nesciunt alias exercitationem nostrum perspiciatis ad quisquam minus officiis? Tenetur, esse. Voluptas magnam aliquid quis dolore ipsa.
+</div>
