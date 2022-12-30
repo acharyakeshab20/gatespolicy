218f3cf5044e8cae55543b5f2791a847f5665ba8
Keshav acharya <acharyakeshab20@gmail.com>
2022-12-30 16:34:04 +0545

    changing things in products

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.cms');
 <div class="container">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quos suscipit nemo aperiam a, eum officia accusantium consequuntur facere nam ipsum corrupti optio nesciunt? Incidunt fugiat tempore distinctio animi aspernatur!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores qui officiis cumque excepturi enim nihil et tempore. Aspernatur aut incidunt ducimus soluta nobis? Velit dolor vel aspernatur minus impedit in.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nobis eaque doloribus odit. Explicabo rem voluptatem, perferendis culpa nihil velit dicta libero nesciunt at reiciendis quod suscipit optio magni nisi?
+    ibus odit. Explicabo rem voluptatem, perferendis culpa nihil velit dicta libero nesciunt at reiciendis quod suscipit optio magni nisi?
+    <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde reprehenderit porro accusamus voluptatem nemo, adipisci veritatis, quasi reiciendis odio autem a nostrum! Similique voluptate rem veniam cupiditate quasi, alias autem?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui sapiente provident reiciendis ad exercitationem atque eligendi quod! Officia dignissimos, culpa magni animi ea, rerum, nobis at earum cupiditate quam expedita?
+    </li>
 </div>
